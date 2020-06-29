7bca94371399e3185e0c629568a24a69354fccbb
ArvindRamesh <aravindk@buffalo.edu>
2020-06-28 21:33:02 -0400

    deployed contract

--- project-3-coffee-supply-chain-dapp/Readme.md
+++ project-3-coffee-supply-chain-dapp/Readme.md
@@ -20,3 +20,6 @@
 - solidity-version: `0.6.2`
 
 #### Contract Details 
+- contract address: `0xf7DD4c62aA0a2647f279CBe275114409a080534D`
+- transaction hash: `0x5d6da03b47ec732048fb0be2cad6aba1b261cf9fa5766f7c0378e522ab700357` 
+- Etherscan: https://rinkeby.etherscan.io/address/0xf7DD4c62aA0a2647f279CBe275114409a080534D
--- project-3-coffee-supply-chain-dapp/truffle.js
+++ project-3-coffee-supply-chain-dapp/truffle.js
@@ -1,10 +1,20 @@
+const infuraKey = "e3d89bd404b343a48f388df4ab74bb41";
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider("23E51D9ED43E656832403859A33B58D800F47EB81C477F3E1AF4ADFA4C341599", `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000,
+        from: "0x0b362f37B06F0017548FB4D637B09cEAEb034763"
+    },
   },
    // Configure your compilers
    compilers: {
