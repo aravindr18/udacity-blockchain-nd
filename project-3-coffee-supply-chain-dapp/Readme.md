90b169d79d258975ab182598b6012838ff7d6281
Aravind Ramesh <aravindk@buffalo.edu>
2020-06-28 21:33:23 -0400

    Project 3 (#9)
    * initial version of project-3
    
    * UMLs
    
    * updated readme
    
    * deployed contract

---
+++ project-3-coffee-supply-chain-dapp/Readme.md
@@ -0,0 +1,25 @@
+## Coffee Supply Chain Using Ethereum and Solidity 
+#### UML 
+##### Activity 
+![Activity](images/Activity.png)
+##### Sequence
+![Sequence](images/Sequence.png)
+##### State
+![State](images/State.png)
+##### Class
+![Class](images/Class.png)
+
+#### Libraries
+- truffle: `v5.1.31`
+- node: `v8.9.4`
+- ganache-cli: `^6.10.0-beta.2`
+- truffle-hdwallet-provider: `^1.0.17`
+- web3-eth: `^1.2.9`
+- web3: `1.2.1`
+- truffle-assertions: `^0.9.2`
+- solidity-version: `0.6.2`
+
+#### Contract Details 
+- contract address: `0xf7DD4c62aA0a2647f279CBe275114409a080534D`
+- transaction hash: `0x5d6da03b47ec732048fb0be2cad6aba1b261cf9fa5766f7c0378e522ab700357` 
+- Etherscan: https://rinkeby.etherscan.io/address/0xf7DD4c62aA0a2647f279CBe275114409a080534D
---
+++ project-3-coffee-supply-chain-dapp/truffle.js
@@ -0,0 +1,33 @@
+const infuraKey = "e3d89bd404b343a48f388df4ab74bb41";
+const HDWalletProvider = require('truffle-hdwallet-provider');
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider("23E51D9ED43E656832403859A33B58D800F47EB81C477F3E1AF4ADFA4C341599", `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000,
+        from: "0x0b362f37B06F0017548FB4D637B09cEAEb034763"
+    },
+  },
+   // Configure your compilers
+   compilers: {
+    solc: {
+      version: "0.6.2",    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
+  }
+};
